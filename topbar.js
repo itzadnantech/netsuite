899cee04d94fab6cc81acb628a32af44e8d1c39a
Adnan Hussain <itzadnantech@gmail.com>
2022-07-15 08:08:42 +0500

    version 1

---
+++ topbar.js
@@ -0,0 +1,16 @@
+$('.social_icons').remove();
+
+///laptop
+var linkedin_li = '<li style="margin-left:10px" class="social_icons"><a href="https://www.linkedin.com/company/lynn-medical/" style="width:50px" title="Linkedin" target="_blank"><img style="vertical-align: middle;" src="https://6761736.app.netsuite.com/core/media/media.nl?id=218137&c=6761736&h=f0cw7L61v5DDkiTS8hiZVpve8q8ad39fZ0XZepekmshpMdsT&fcts=20220713211439&whence="></a></li>';
+var facebook_li = '<li style="margin-left:-8px" class="social_icons"><a href="https://www.facebook.com/lynnmedical" style="width:50px" title="Facebook" target="_blank"><img style="vertical-align: middle;" src="https://6761736.app.netsuite.com/core/media/media.nl?id=218138&c=6761736&h=gjlz-J4ZzengqlJQYC_oSxIMcVySosd1Zcb277rlQjE6d-6S&fcts=20220714040341&whence="></a></li>';
+$('.header-subheader-options').append(linkedin_li);
+$('.header-subheader-options').append(facebook_li);
+
+////mobile 
+var mob_linkedin_li = '<li style="margin-left:10px" class="social_icons"><a href="https://www.linkedin.com/company/lynn-medical/" style="width:68px" title="Linkedin" target="_blank"><img style="vertical-align: middle;" src="https://6761736.app.netsuite.com/core/media/media.nl?id=218140&c=6761736&h=meKii9urrasS-WOcjkwOshl4iD0V0WJo1iaaedNHrIncDeJD&fcts=20220714044130&whence="></a></li>';
+var mob_facebook_li = '<li style="margin-left:10px" class="social_icons"><a href="https://www.facebook.com/lynnmedical" style="width:68px" title="Facebook" target="_blank"><img style="vertical-align: middle;" src="https://6761736.app.netsuite.com/core/media/media.nl?id=218139&c=6761736&h=6RvMxvHW16nFrDNnD4pACRTJpwl4zWgNEFsx1Ntezh9iDxMW&fcts=20220714044103&whence="></a></li>';
+$('.header-sidebar-menu').append(mob_linkedin_li);
+$('.header-sidebar-menu').append(mob_facebook_li);
+
+// var test = '<li data-view="QuickOrderHeaderLink"><!-- TEMPLATE STARTS: quickorder_accesspoints_headerlink--><a class="quickorder-accesspoints-headerlink-link" href="#" data-touchpoint="home" data-hashtag="#cart?openQuickOrder=true" title="Quick Order"> <img src="https://6761736.app.netsuite.com/core/media/media.nl?id=218136&c=6761736&h=54j8j8Imz7rKpyyJ8tn2N7K8X2zRstUlA6h8VGnw_MO5FnxO&fcts=20220713211423&whence="> </a><!-- TEMPLATE ENDS: quickorder_accesspoints_headerlink --></li>'
+// $('.header-subheader-options').append(test);
